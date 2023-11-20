15aa781fb2dcc57ce2c837bb8ed92b3e1652016c
acJoaog <joao.jg@ges.inatel.br>
2023-11-20 20:39:21 -0300

    Aula 6

---
+++ aula_karate/src/test/java/aula_inatel/pokemon/pokeRunner.java
@@ -0,0 +1,11 @@
+package aula_inatel.pokemon;
+
+import com.intuit.karate.junit5.Karate;
+
+public class pokeRunner {
+    @Karate.Test
+    Karate testStarWars() {
+        return Karate.run("pokemon").relativeTo(getClass());
+    }    
+
+}
